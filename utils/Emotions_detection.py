100173b43c5f14946a0600fc3872248b3ce37fdf
Oussema Hedri <oussemahedri@gmail.com>
2020-05-02 05:43:35 +0200

    changes

--- utils/Emotions_detection.py
+++ utils/Emotions_detection.py
@@ -1,62 +1,37 @@
 from keras.preprocessing.image import img_to_array
-#import imutils
 import cv2
 from keras.models import load_model
 import numpy as np
 
-
-detection_model_path = 'cascades/data/haarcascade_frontalface_default.xml'
-emotion_model_path = 'Emotions.h5'
-
+detection_model_path = 'C:/Users/Ousse/Desktop/Projects/Emotion Detector/utils/cascades/data/haarcascade_frontalface_default.xml'
+emotion_model_path = 'C:/Users/Ousse/Desktop/Projects/Emotion Detector/utils/Emotions.h5'
 
 face_detection = cv2.CascadeClassifier(detection_model_path)
 emotion_classifier = load_model(emotion_model_path, compile=False)
-EMOTIONS = ["angry" ,"happy","surprised"]
+EMOTIONS = ["angry","happy","surprised"]
 
-
-
-cv2.namedWindow('test')
+cv2.namedWindow('face')
 camera = cv2.VideoCapture(0)
 while True:
     frame = camera.read()[1]
-    
-    faces = face_detection.detectMultiScale(frame,scaleFactor=1.05, minNeighbors=1)
-    
-    canvas = np.zeros((250, 300, 3), dtype="uint8")
-    frameClone = frame.copy()
-    if len(faces) > 0:
-        faces = sorted(faces, reverse=True,
-        key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
-        (fX, fY, fW, fH) = faces
+    faces = face_detection.detectMultiScale(frame,scaleFactor=1.4, minNeighbors=1,minSize=(30, 30))
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi = gray[y:y+h, x:x+w]
         roi = frame[fY:fY + fH, fX:fX + fW]
         roi = cv2.resize(roi, (48, 48))
         roi = roi.astype("float") / 255.0
         roi = img_to_array(roi)
         roi = np.expand_dims(roi, axis=0)
-        
-        
         preds = emotion_classifier.predict(roi)[0]
         emotion_probability = np.max(preds)
         label = EMOTIONS[preds.argmax()]
-
- 
-    for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
-                text = "{}: {:.2f}%".format(emotion, prob * 100)
-                w = int(prob * 300)
-                cv2.rectangle(canvas, (7, (i * 35) + 5),
-                (w, (i * 35) + 35), (0, 0, 255), -1)
-                cv2.putText(canvas, text, (10, (i * 35) + 23),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45,
-                (255, 255, 255), 2)
-                cv2.putText(frameClone, label, (fX, fY - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-                cv2.rectangle(frameClone, (fX, fY), (fX + fW, fY + fH),
-                              (0, 0, 255), 2)
-
-    cv2.imshow('your_face', frameClone)
-    cv2.imshow("Probabilities", canvas)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        font = cv2.FONT_HERSHEY_SIMPLEX
+    	color = (255, 255, 255)
+    	stroke = 2
+    	cv2.putText(frame, label, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 camera.release()
 cv2.destroyAllWindows()
